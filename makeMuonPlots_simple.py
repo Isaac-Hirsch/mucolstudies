eeaf3f02176d6100f464882fb63cef393c8a5820
Isaac Hirsch <isaac@Isaacs-MacBook-Air.local>
2023-07-17 20:27:10 -0500

    trying to find relations in IB hits

--- makeMuonPlots_simple.py
+++ makeMuonPlots_simple.py
@@ -1,17 +1,57 @@
+from array import array
 import pyLCIO
 import ROOT
+from ROOT import TH1D, TH2D, TFile, TLorentzVector, TTree, TMath
 import glob
+from optparse import OptionParser
+
+#Boilerplate added for downloads
+Bfield = 3.56  # T
+parser = OptionParser()
+parser.add_option('-i', '--inFile', help='--inFile Output_REC.slcio',
+                  type=str, default='Output_REC.slcio')
+parser.add_option('-o', '--outFile', help='--outFile ntup_tracks',
+                  type=str, default='ntup_tracks')
+(options, args) = parser.parse_args()
+
+tree = TTree("tracks_tree", "tracks_tree")
+
+#Initialize the branches for the info to be stored in
+pt = array('d', [0])
+pt_truth = array('d', [0])
+phi = array('d', [0])
+theta = array('d', [0])
+d0 = array('d', [0])
+z0 = array('d', [0])
+z_truth = array('d', [0])
+sigma_d0 = array('d', [0])
+sigma_z0 = array('d', [0])
+omega = array('d', [0])
+chi2 = array('d', [0])
+ndf = array('i', [0])
+nhits = array('i', [0])
+pdgID = array('i', [0])
+
+tree.Branch("pT",  pt,  'var/D')
+tree.Branch("pTtruth",  pt_truth,  'var/D')
+tree.Branch("phi", phi, 'var/D')
+tree.Branch("theta", theta, 'var/D')
+tree.Branch("d0", d0, 'var/D')
+tree.Branch("z0", z0, 'var/D')
+tree.Branch("z_truth", z_truth, 'var/D')
+tree.Branch("sigma_d0", sigma_d0, 'var/D')
+tree.Branch("sigma_z0", sigma_z0, 'var/D')
+tree.Branch("omega", omega, 'var/D')
+tree.Branch("chi2", chi2, 'var/D')
+tree.Branch("ndf", ndf, 'var/I')
+tree.Branch("nhits", nhits, 'var/I')
+tree.Branch("pdgID", pdgID, 'var/I')
 
 # Set up some options
 max_events = -1
 
 # Gather input files
 fnames = glob.glob("/data/fmeloni/DataMuC_MuColl_v1/muonGun/reco/*.slcio")
-
-# Set up histograms
-hists = {}
-hists["pfo_pt"] = ROOT.TH1F("pfo_pt", "pfo_pt", 100, 0, 2000)
-hists["muon_pt"] = ROOT.TH1F("muon_pt", "muon_pt", 100, 0, 2000)
 
 # Loop over events
 i = 0
@@ -21,26 +61,18 @@ for f in fnames:
 
     for event in reader:
         if max_events > 0 and i >= max_events: break
-        if i%100 == 0:
-            print("Processing event %i."%i)
 
         # Get the collections we care about
-        mcpCollection = event.getCollection("MCParticle")
-        pfoCollection = event.getCollection("PandoraPFOs")
-
+        IBH= event.getCollection("IBTrackerHits")
+        IBHR = event.getCollection("IBTrackerHitsRelations")
+        if i%100 == 0:
+            print("Processing event %i."%i)
+            print("IBH:")
+            print(dir(IBH))
+            print("IBHR:")
+            print(dir(IBHR))
         # Loop over the reconstructed objects and fill histograms
-        for pfo in pfoCollection:
-            pfo_p = pfo.getMomentum()
-            pfo_tlv = ROOT.TLorentzVector()
-            pfo_tlv.SetPxPyPzE(pfo_p[0], pfo_p[1], pfo_p[2], pfo.getEnergy())
-            hists["pfo_pt"].Fill(pfo_tlv.Perp())
+        #for pfo in pfoCollection:
 
-            if abs(pfo.getType())==13:
-                hists["muon_pt"].Fill(pfo_tlv.Perp())
         i+=1
 
-# Make your plots
-for i, h in enumerate(hists):
-    c = ROOT.TCanvas("c%i"%i, "c%i"%i)
-    hists[h].Draw()
-    c.SaveAs("%s.png"%h)
