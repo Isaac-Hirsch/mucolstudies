fe0efd7857a5e58739f4e2095619f51e4f680e9c
Isaac Hirsch <isaac-hirsch@login.snowmass21.io>
2023-07-19 10:19:10 -0500

    updates on snowmass needed for group presentation

--- makeMuonPlots_simple.py
+++ makeMuonPlots_simple.py
@@ -10,48 +10,29 @@ Bfield = 3.56  # T
 parser = OptionParser()
 parser.add_option('-i', '--inFile', help='--inFile Output_REC.slcio',
                   type=str, default='Output_REC.slcio')
-parser.add_option('-o', '--outFile', help='--outFile ntup_tracks',
-                  type=str, default='ntup_tracks')
+parser.add_option('-o', '--outFile', help='--outFile ntup_tracks.root',
+                  type=str, default='ntup_tracks.root')
 (options, args) = parser.parse_args()
 
 tree = TTree("tracks_tree", "tracks_tree")
 
 #Initialize the branches for the info to be stored in
-pt = array('d', [0])
-pt_truth = array('d', [0])
-phi = array('d', [0])
-theta = array('d', [0])
-d0 = array('d', [0])
-z0 = array('d', [0])
-z_truth = array('d', [0])
-sigma_d0 = array('d', [0])
-sigma_z0 = array('d', [0])
-omega = array('d', [0])
-chi2 = array('d', [0])
-ndf = array('i', [0])
-nhits = array('i', [0])
-pdgID = array('i', [0])
+x_pos = array('d', [0])
+y_pos = array('d', [0])
+z_pos = array('d', [0])
+time = array ('d', [0])
 
-tree.Branch("pT",  pt,  'var/D')
-tree.Branch("pTtruth",  pt_truth,  'var/D')
-tree.Branch("phi", phi, 'var/D')
-tree.Branch("theta", theta, 'var/D')
-tree.Branch("d0", d0, 'var/D')
-tree.Branch("z0", z0, 'var/D')
-tree.Branch("z_truth", z_truth, 'var/D')
-tree.Branch("sigma_d0", sigma_d0, 'var/D')
-tree.Branch("sigma_z0", sigma_z0, 'var/D')
-tree.Branch("omega", omega, 'var/D')
-tree.Branch("chi2", chi2, 'var/D')
-tree.Branch("ndf", ndf, 'var/I')
-tree.Branch("nhits", nhits, 'var/I')
-tree.Branch("pdgID", pdgID, 'var/I')
+tree.Branch("x",  x_pos,  'var/D')
+tree.Branch("y",  y_pos,  'var/D')
+tree.Branch("z", z_pos, 'var/D')
+tree.Branch("t", time, 'var/D')
 
+print(tree)
 # Set up some options
 max_events = -1
 
 # Gather input files
-fnames = glob.glob("/data/fmeloni/DataMuC_MuColl_v1/muonGun/reco/*.slcio")
+fnames = glob.glob("/data/fmeloni/DataMuC_MuColl_v1/muonGun/recoBIB/*100.slcio")
 
 # Loop over events
 i = 0
@@ -65,14 +46,26 @@ for f in fnames:
         # Get the collections we care about
         IBH= event.getCollection("IBTrackerHits")
         IBHR = event.getCollection("IBTrackerHitsRelations")
-        if i%100 == 0:
-            print("Processing event %i."%i)
-            print("IBH:")
-            print(dir(IBH))
-            print("IBHR:")
-            print(dir(IBHR))
+        tracks=event.getCollection("SiTracks")
+        if i % 100==0:
+            print("processing",i)
+        for track in tracks:
+            hits=track.getTrackerHits()
+            for hit in hits:
+                t=hit.getTime()
+                typ=hit.getType()
+                posV=hit.getPositionVec()
+                x=posV.X()
+                y=posV.Y()
+                z=posV.Z()
+                x_pos[0]=x
+                y_pos[0]=y
+                z_pos[0]=z
+                time[0]=t
+                tree.Fill()
         # Loop over the reconstructed objects and fill histograms
         #for pfo in pfoCollection:
 
         i+=1
-
+output_file = TFile(options.outFile, 'RECREATE')
+tree.Write()
