a3f0b81597ab1e0c28c3000671ce241f7dc15ab0
Isaac Hirsch <isaac@Isaacs-MacBook-Air.local>
2023-07-24 11:06:38 -0500

    changed linspace to always have the correct differences in times while accounting for hte minimum and max time of hits

--- TimeWindowAnalysis.py
+++ TimeWindowAnalysis.py
@@ -6,10 +6,16 @@ from scipy.stats import norm
 
 treeBIB=uproot.open("/work/isaac-hirsch/mucolstudies/ntup_hits.root")["tracks_tree"]
 
-vertStartTime=norm.ppf(q=np.linspace(norm.cdf(-0.1, scale=0.03),0.20,20),scale=0.03)
-vertEndTime=norm.ppf(q=np.linspace(0.80,norm.cdf(0.15, scale=0.03),20),scale=0.03)
-innerOuterStartTime=norm.ppf(q=np.linspace(norm.cdf(-0.2, scale=0.06),0.20,20),scale=0.06)
-innerOuterEndTime=norm.ppf(q=np.linspace(0.80,norm.cdf(0.3, scale=0.06),20),scale=0.06)
+percentile=0.01 #write in fractional form (0<=percentile<=1)
+bins=20
+
+#time series has a hard start time at -0.1 ns in the vertex and -0.2 ns in the inner and outer detectors. 
+#It also has a hard end time of 0.15 ns in the vertex and 0.3 ns in the inner and outer detectors
+#The bellow code was calculated by setting these as the start for the StartTime and end for EndTime, then making sure there was a difference of 1-percentile between every pair of linspace points
+vertStartTime=norm.ppf(q=np.linspace(norm.cdf(-0.1, scale=0.03),percentile-1+norm.cdf(0.15, scale=0.03),bins),scale=0.03)
+vertEndTime=norm.ppf(q=np.linspace(1-percentile+norm.cdf(-0.1, scale=0.03),norm.cdf(0.15, scale=0.03),bins),scale=0.03)
+innerOuterStartTime=norm.ppf(q=np.linspace(norm.cdf(-0.2, scale=0.06),percentile-1+norm.cdf(0.3, scale=0.06),bins),scale=0.06)
+innerOuterEndTime=norm.ppf(q=np.linspace(1-percentile+norm.cdf(-0.2, scale=0.06),norm.cdf(0.3, scale=0.06),bins),scale=0.06)
 
 layerPerModule={
     1 : 8,
