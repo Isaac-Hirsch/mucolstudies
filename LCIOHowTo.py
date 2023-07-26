703f51d20463f10889ac5d4e3bd26e27c1f2f2c0
Isaac Hirsch <isaac@Isaacs-MacBook-Air.local>
2023-07-26 15:33:18 -0500

    more updates since running last file did not print

--- LCIOHowTo.py
+++ LCIOHowTo.py
@@ -6,13 +6,14 @@ from ROOT import TH1D, TH2D, TFile, TLorentzVector, TTree, TMath
 import glob
 from optparse import OptionParser
 
-fnames = glob.glob("/data/fmeloni/DataMuC_MuColl_v1/muonGun/reco/muonGun_reco_660.slcio")
+fnames = glob.glob("/data/fmeloni/DataMuC_MuColl_v1/muonGun/reco/muonGun_reco_6[56]0.slcio")
 
+i = 0
 for f in fnames:
+    print(i)
     reader = IOIMPL.LCFactory.getInstance().createLCReader()
     reader.open(f)
 
-    i = 0
     print("Reader")
     print(dir(reader))
     print("")
