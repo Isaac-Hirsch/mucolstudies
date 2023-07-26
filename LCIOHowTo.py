ffd3ff1e128022dd4548651c662f02451060fd33
Isaac Hirsch <isaac@Isaacs-MacBook-Air.local>
2023-07-26 15:28:47 -0500

    made changes that added back in glob.glob since the other version was not working

--- LCIOHowTo.py
+++ LCIOHowTo.py
@@ -6,27 +6,28 @@ from ROOT import TH1D, TH2D, TFile, TLorentzVector, TTree, TMath
 import glob
 from optparse import OptionParser
 
-fnames = "/data/fmeloni/DataMuC_MuColl_v1/muonGun/reco/muonGun_reco_660.slcio"
+fnames = glob.glob("/data/fmeloni/DataMuC_MuColl_v1/muonGun/reco/muonGun_reco_660.slcio")
 
-reader = IOIMPL.LCFactory.getInstance().createLCReader()
-reader.open(f)
+for f in fnames:
+    reader = IOIMPL.LCFactory.getInstance().createLCReader()
+    reader.open(f)
 
-i = 0
-print("Reader")
-print(dir(reader))
-print("")
-for event in reader:
-    if i ==0:
-        print("event")
-        print(dir(event))
-        print("")
-        print("Collection")
-        print(dir(event.getCollection("VBTrackerHits")))
-        j=0
-        for hit in event.getCollection("VBTrackerHits"):
-            if j==0:
-                print("Hit")
-                print(dir(hit))
-                print("")
-                j+=1
-    i+=1
+    i = 0
+    print("Reader")
+    print(dir(reader))
+    print("")
+    for event in reader:
+        if i ==0:
+            print("event")
+            print(dir(event))
+            print("")
+            print("Collection")
+            print(dir(event.getCollection("VBTrackerHits")))
+            j=0
+            for hit in event.getCollection("VBTrackerHits"):
+                if j==0:
+                    print("Hit")
+                    print(dir(hit))
+                    print("")
+                    j+=1
+        i+=1
